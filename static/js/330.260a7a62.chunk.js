f92be3ca22cd505e4f32b66acecb9d716df776ff
iraboldyrieva <iraboldyrieva@gmail.com>
2024-06-17 15:04:32 +0300

    Clean previous build

--- static/js/330.260a7a62.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktodo_app=self.webpackChunktodo_app||[]).push([[330],{330:(r,e,n)=>{n.r(e),n.d(e,{default:()=>c});n(43);var o=n(216),t=n(436),a=n(579);function c(){const r=(0,o.Zp)();return(0,a.jsxs)("div",{className:t.A.container,children:[(0,a.jsx)("h1",{children:"Page is not found"}),(0,a.jsx)("button",{onClick:()=>r("/"),className:t.A.errorBtn,children:"Back to Home page"})]})}},436:(r,e,n)=>{n.d(e,{A:()=>o});const o={container:"ErrorPage_container__BKNmU",errorBtn:"ErrorPage_errorBtn__RwG0C"}}}]);
-//# sourceMappingURL=330.260a7a62.chunk.js.map
